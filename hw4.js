e8a14eec6663ac403226da85c6b3941687b84dab
An1rey <andrey.ignatuk567@gmail.com>
2022-12-02 13:04:18 +0200

    Arithmetic mean

---
+++ hw4.js
@@ -0,0 +1,6 @@
+const  a = +prompt("Користувач вводить одне число");
+const  b = +prompt("Користувач вводить друге число");
+const  c = +prompt("Коритувач вводить треье число");
+console .log(a,b,c);
+const  x = (a+b+ +c)/3;
+alert(x)
