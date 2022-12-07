82901f0d5ca1755a4f1eea0df9574991ac39759f
An1rey <andrey.ignatuk567@gmail.com>
2022-12-07 18:38:19 +0200

    added some changes

--- hw5.js
+++ hw5.js
@@ -1,26 +1,32 @@
-const age = prompt("запитати у користувача рік народження.");
-const city = prompt("запитати в нього, в якому місті він живе.");
-const sport = prompt("запитати його улюблений вид спорту.");
-const sport_1 = "basketball";
-const sport_2 ="football";
-const sport_3 = "box";
-const champion_basketball = "Kobe Bryant";
-const champion_football = "Lionel Messi";
-const champion_box = "Oleksandr Usyk";
-let cityMessage;
-alert("Мені " +age+ " років")
-if(city === "Київ" || city === "Вашингтон" ){
-   alert("Ти живеш у столиці? " + city);
+const birthday = prompt('Год рождения')
+const city = prompt('Город')
+const sport = prompt('Спорт')
+const todayDate= new Date()
+
+const age = todayDate.getFullYear() - Number(birthday)
+
+const capitalArr = ["Киев", 'London','Вашингтонн']
+const sportArr = ['бокс', 'футболл', 'баскетбол']
+
+const nameSportsmanObj={
+    'бокс':'Тайсонн',
+    'футболл':'Messi',
+    'баскетбол' : 'Kobye'
 }
-if (city === "Вашингтон" || city === "Київ" ){
-    alert("Ти живешь у місті? " + city);
+const boxSport='Тайсон'
+
+const checkFuncByParam =(willCheckItem)=> (it) => it === willCheckItem
+
+const showCityMessage = () => {
+    capitalArr.some(checkFuncByParam(city)) ? alert(`Вы живете в столице ${city}`):alert(`Вы живете в городе ${city}`)
 }
-if(sport === sport_1){
-    alert("Круто! Хочеш стати " + champion_basketball)
+
+const showSportMessage=()=>{
+    sportArr.some(checkFuncByParam(sport)) ? alert(`Круто ты хочешь стать как ${nameSportsmanObj[sport]}`) : null
 }
-if(sport === sport_2){
-    alert("Круто! Хочеш стати " + champion_football)
-}
-if(sport === sport_3){
-    alert("Круто! Хочеш стати "  + champion_box)
-}
+
+
+age ? alert(`Ваш возраст ${age}`) : null
+
+showCityMessage()
+showSportMessage()
