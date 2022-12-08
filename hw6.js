b66c27b07f634a295d0dbd9f8c44d8eb0f8dab25
An1rey <andrey.ignatuk567@gmail.com>
2022-12-08 10:38:21 +0200

    learn switch cases

---
+++ hw6.js
@@ -0,0 +1,15 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+switch (numOrStr){
+    case '' :
+        console.log('ви скасували')
+        break
+    case isNaN (+ numOrStr):
+        console.log(' number is Ba_NaN')
+        break
+    case  null :
+        console.log('Empty String');
+        break
+        default:
+            console.log('OK!')
+}
