af0873eddc7d8bba849f4479fb5efc118c73dd3a
An1rey <andrey.ignatuk567@gmail.com>
2022-12-26 11:52:26 +0200

    fix number

--- hm9.js
+++ hm9.js
@@ -1,12 +1,12 @@
 const arraySize = +prompt('Enter a digit og Array')
-let array = [];
-for (let i = 0 ; i  < arraySize; i=i+1){
-    let digit = parseInt(prompt('Enter digit #' + (i + 1)));
+const array = [];
+for (let i = 0 ; i  < arraySize; i++){
+    let digit = +(prompt('Enter digit #' + (i + 1)));
     array.push(digit);
 }
 console.log(array);
 
-array.sort();
+Array.prototype.sort();
 console.log(array);
 
 array.splice(1,3);
